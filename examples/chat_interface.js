abbc4239fddfdc759bdaeacbcbf995cd8fb3107d
MeDott29 <mhendricks1290@gmail.com>
2025-02-01 22:55:21 -0500

    add chat interface examples

---
+++ examples/chat_interface.js
@@ -0,0 +1,46 @@
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+  systemInstruction: "test system message",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+      {
+        role: "user",
+        parts: [
+          {text: "test user message"},
+        ],
+      },
+      {
+        role: "model",
+        parts: [
+          {text: "Okay, I've received your \"test user message\". How can I help you today?\n"},
+        ],
+      },
+    ],
+  });
+
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  console.log(result.response.text());
+}
+
+run();
